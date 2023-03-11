6f1825cdd8c1ed21b0d61ecd5db8546694c11f83
Jason Huang <jason97521@gmail.com>
2023-03-11 11:13:04 -0500

    local storage with context api

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -7,7 +7,7 @@ import "./login.css";
 export default function Login() {
   const userRef = useRef();
   const passwordRef = useRef();
-  const { user, dispatch, isFetching } = useContext(Context);
+  const { dispatch, isFetching } = useContext(Context);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -23,7 +23,6 @@ export default function Login() {
     }
   };
 
-  console.log(user)
   return (
     <div className="login">
       <span className="loginTitle">Login</span>
